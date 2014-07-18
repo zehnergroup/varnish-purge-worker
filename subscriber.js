37f3a629c233ddd87fdaf271030fa7d231a41627
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-07-18 01:19:50 -0300

    [RDI-407] Replace 'colors' module with 'chalk'

--- subscriber.js
+++ subscriber.js
@@ -1,21 +1,17 @@
-var colors = require('colors'),
-    fs = require('fs'),
-    config = fs.readFileSync('./config.json');
+var fs = require('fs'),
+    config = fs.readFileSync('./config.json'),
+    chalk = require('chalk'),
+    info = chalk.green,
+    warn = chalk.yellow,
+    debug = chalk.blue,
+    error = chalk.red;
 
-colors.setTheme({
-  info: 'green',
-  data: 'blue',
-  warn: 'yellow',
-  debug: 'blue',
-  error: 'red'
-});
-
-console.log('Loading configuration from file.'.info);
+console.log( info('Loading configuration from file.') );
 try {
   config = JSON.parse(config);
 } catch (err) {
-  console.log('There has been an error parsing the configuration file (config.json).'.error)
-  console.log( ('' + err).warn );
+  console.log( error('There has been an error parsing the configuration file (config.json).') );
+  console.log( warn('' + err) );
   process.exit(1);
 }
 
@@ -24,10 +20,10 @@ var redis = require("redis"),
     varnish = require('varnish'),
     varnish_admin = new varnish.Admin(config.varnish.host, config.varnish.port, {file: config.varnish.secret_file});
 
-console.log('Subscribe to Redis channel.'.info);
+console.log( info('Subscribe to Redis channel.') );
 redis_client.subscribe(config.redis.channel);
 
-console.log('Listening for Redis messages.'.info);
+console.log( info('Listening for Redis messages.') );
 redis_client.on("message", function (channel, message) {
 
   var routes = (JSON.parse(message)).routes;
@@ -36,11 +32,11 @@ redis_client.on("message", function (channel, message) {
 
     for (i = 0; i < routes.length; i++) {
 
-      console.log( ('ban.url ' + routes[i]).debug );
+      console.log( debug('ban.url ' + routes[i]) );
 
       varnish_admin.send('ban.url ' + routes[i], function(err, resp){
-        if(err) return console.log('Varnish command failed'.error);
-        console.log( ('Varnish response ' + resp).debug );
+        if(err) return console.log( error('Varnish command failed') );
+        console.log( debug('Varnish response ' + resp) );
       });
 
     }
